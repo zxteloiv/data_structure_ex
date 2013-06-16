3b25561724231004ef27087c9f34de535b27d263
Shiina Luce <zxteloiv@gmail.com>
2013-06-16 22:18:18 +0800

    Add Size() API and the other 2 stack implementations

--- algo_sedgewick/stacks.py
+++ algo_sedgewick/stacks.py
@@ -21,12 +21,14 @@ class ListStack:
 
     def __init__(self):
         self.head = self.Node()
+        self.size = 0
 
     def Push(self, val):
         newNode = self.Node()
         newNode.val = val
         newNode.next = self.head.next
         self.head.next = newNode
+        self.size = self.size + 1
         return self
 
     def Pop(self):
@@ -34,8 +36,73 @@ class ListStack:
             return None
         val = self.head.next.val
         self.head = self.head.next
+        self.size = self.size - 1
         return val
 
     def IsEmpty(self):
         return self.head.next == None
 
+    def Size(self):
+        return self.size
+
+# 2. implement a stack using an array
+class ArrayStack:
+    def __init__(self, stack_size=10):
+        self.stack = range(stack_size)
+        self.usage = 0
+        self.stack_size = stack_size
+        
+    def Push(self, val):
+        # list index starts from 0
+        self.stack[self.usage] = val
+        self.usage = self.usage + 1
+
+        # extend(double) the size of stack when it is full
+        if self.usage == self.stack_size:
+            new_stack = range(2 * self.stack_size)
+            new_stack[0:self.usage] = self.stack
+            self.stack = new_stack
+            self.stack_size = self.stack_size * 2
+        return self
+
+    def Pop(self):
+        if self.usage == 0:
+            return None
+        val = self.stack[self.usage - 1]
+        self.usage = self.usage - 1
+
+        # shrink(halve) the size of stack when it is 1/4 full
+        # but at least 10 room is reserved
+        if self.usage <= self.stack_size / 4 and self.stack_size > 10:
+            new_stack = range(self.stack_size / 2)
+            new_stack[0:self.usage] = self.stack[0:self.usage]
+            self.stack = new_stack
+            self.stack_size = self.stack_size / 2
+        return val
+
+    def IsEmpty(self):
+        return self.usage == 0
+
+    def Size(self):
+        return self.usage
+
+    def ReservedSize(self):
+        return self.stack_size
+
+# 3. implement a stack using python list in a easier way
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def Push(self, val):
+        self.stack.append(val)
+        return self
+
+    def Pop(self, val):
+        return self.stack.pop()
+
+    def IsEmpty(self):
+        return 0 == len(self.stack)
+
+    def Size(self):
+        return len(self.stack)
--- algo_sedgewick/stacks_test.py
+++ algo_sedgewick/stacks_test.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python2
 
 import unittest
-from stacks import ListStack
+from stacks import ListStack, ArrayStack, Stack
 
 class TestListStack(unittest.TestCase):
     def setUp(self):
-        #print 'Run test setUp method...'
-        pass
+        print '\nRun test for ListStack'
 
     def test_init(self):
         s = ListStack()
@@ -47,6 +46,100 @@ class TestListStack(unittest.TestCase):
         self.assertEqual(s.Pop(), 10)
         self.assertEqual(s.Pop(), None)
         self.assertTrue(s.IsEmpty())
-        
+
+    def test_size(self):
+        s = ListStack()
+        self.assertEqual(s.Size(), 0)
+        s.Push(10)
+        self.assertEqual(s.Size(), 1)
+        s.Pop()
+        self.assertEqual(s.Size(), 0)
+        s.Push(10)
+        s.Push(10)
+        s.Push(10)
+        self.assertEqual(s.Size(), 3)
+        s.Push(10)
+        self.assertEqual(s.Size(), 4)
+
+class TestArrayStack(unittest.TestCase):
+    def setUp(self):
+        print '\nRun test for ArrayStack\n'
+
+    def test_init(self):
+        s = ArrayStack(20)
+        l = ArrayStack()
+
+    def test_empty(self):
+        s = ArrayStack(20)
+        l = ArrayStack()
+        self.assertTrue(s.IsEmpty())
+        self.assertTrue(l.IsEmpty())
+
+    def test_push(self):
+        s = ArrayStack(5)
+        s.Push(100)
+        self.assertTrue(not s.IsEmpty())
+        s.Push(200)
+        self.assertTrue(not s.IsEmpty())
+        s.Push(300).Push(400)
+        self.assertTrue(not s.IsEmpty())
+
+    def test_pop(self):
+        s = ArrayStack()
+        self.assertTrue(s.IsEmpty())
+        self.assertEqual(s.Pop(), None)
+        s.Push(10)
+        self.assertTrue(not s.IsEmpty())
+        self.assertEqual(s.Pop(), 10)
+        s.Push(10).Push(20).Push(30)
+        self.assertEqual(s.Pop(), 30)
+        s.Push(40)
+        self.assertEqual(s.Pop(), 40)
+        self.assertTrue(not s.IsEmpty())
+        self.assertEqual(s.Pop(), 20)
+        self.assertTrue(not s.IsEmpty())
+        self.assertEqual(s.Pop(), 10)
+        self.assertTrue(s.IsEmpty())
+        self.assertEqual(s.Pop(), None)
+
+    def test_size(self):
+        s = ArrayStack()
+        self.assertEqual(s.Size(), 0)
+        s.Push(10)
+        self.assertEqual(s.Size(), 1)
+        s.Push(10)
+        self.assertEqual(s.Size(), 2)
+        self.assertEqual(s.Pop(), 10)
+        self.assertEqual(s.Size(), 1)
+        self.assertEqual(s.Pop(), 10)
+        self.assertEqual(s.Size(), 0)
+
+    def test_reserved_size(self):
+        s = ArrayStack()
+        self.assertEqual(s.ReservedSize(), 10)
+        s = ArrayStack(20)
+        self.assertEqual(s.ReservedSize(), 20)
+        s = ArrayStack(5)
+        self.assertEqual(s.ReservedSize(), 5)
+        s.Push(10).Push(20)
+        self.assertEqual(s.ReservedSize(), 5)
+        s.Push(30).Push(40)
+        self.assertEqual(s.ReservedSize(), 5)
+        s.Push(50)
+        self.assertEqual(s.ReservedSize(), 10)
+        s.Push(60)
+        self.assertEqual(s.ReservedSize(), 10)
+        s.Push(70).Push(80).Push(90).Push(100)
+        self.assertEqual(s.ReservedSize(), 20)
+        s.Pop(); s.Pop(); s.Pop(); s.Pop();
+        self.assertEqual(s.ReservedSize(), 20)
+        s.Pop()
+        self.assertEqual(s.ReservedSize(), 10)
+        s.Pop()
+        self.assertEqual(s.ReservedSize(), 10)
+        s.Pop(); s.Pop(); s.Pop(); s.Pop();
+        self.assertEqual(s.ReservedSize(), 10)
+        self.assertEqual(s.Pop(), None)
+
 if __name__ == '__main__':
     unittest.main()
