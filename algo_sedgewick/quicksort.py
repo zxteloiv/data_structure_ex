17591c06bb71c8836508e03ae2459721e342c6c3
Shiina Luce <zxteloiv@gmail.com>
2013-07-04 00:08:48 +0800

    add raw quicksort implementation

--- algo_sedgewick/quicksort.py
+++ algo_sedgewick/quicksort.py
@@ -28,4 +28,29 @@ class QSort3Way:
     def Sort(arr, len):
         QSort3Way._sortOnce(arr, 0, len - 1)
 
+class QSort:
+    @staticmethod
+    def _partition(arr, start, end):
+        cur1, cur2 = start, end
+        while True:
+            while cur1 <= end and arr[cur1] < arr[start]: cur1 += 1
+            while cur2 >= start and arr[cur2] > arr[start]: cur2 -= 1
+            if cur1 >= cur2: break
+            swap(arr, cur1, cur2)
+            cur1 += 1
+            cur2 -= 1
+
+        swap(arr, cur2, start)
+        return cur2
+
+    @staticmethod
+    def _sort(arr, start, end):
+        if start >= end: return
+        pos = QSort._partition(arr, start, end)
+        QSort._sort(arr, start, pos - 1)
+        QSort._sort(arr, pos + 1, end)
+
+    @staticmethod
+    def Sort(arr, len):
+        QSort._sort(arr, 0, len - 1)
 
--- algo_sedgewick/quicksort_test.py
+++ algo_sedgewick/quicksort_test.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python2
 
 import unittest
-from quicksort import QSort3Way
+from quicksort import QSort3Way, QSort
 from random import sample
 from elementary_sort_test import SortTester
 
-class TestSelectionSort(SortTester):
+class TestQSort3Way(SortTester):
     def setUp(self):
         print "\nRun test for QSort3Way"
 
@@ -29,6 +29,33 @@ class TestSelectionSort(SortTester):
         arr = sample(range(1,100), 20)
         arr.extend([50] * 10)
         QSort3Way.Sort(arr, len(arr))
+        print arr
+        self.checkArrayAscending(arr)
+
+class TestQSort(SortTester):
+    def setUp(self):
+        print "\nRun test for QSort"
+
+    def test_sort(self):
+        arr = range(10)
+        QSort.Sort(arr, len(arr))
+        print arr
+        self.checkArrayAscending(arr)
+        arr = [20, 18, 17, 16, 15, 14, 13, 12, 11, 10]
+        QSort.Sort(arr, len(arr))
+        print arr
+        self.checkArrayAscending(arr)
+        arr = [10, 20, 30, 40, 50, 11, 22, 33, 44, 55]
+        QSort.Sort(arr, len(arr))
+        print arr
+        self.checkArrayAscending(arr)
+        arr = [10, 20, 10, 40, 10, 11, 22, 10, 44, 10]
+        QSort.Sort(arr, len(arr))
+        print arr
+        self.checkArrayAscending(arr)
+        arr = sample(range(1,100), 20)
+        arr.extend([50] * 10)
+        QSort.Sort(arr, len(arr))
         print arr
         self.checkArrayAscending(arr)
 
